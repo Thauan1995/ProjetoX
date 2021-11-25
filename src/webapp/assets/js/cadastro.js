b94abecbdf020bc94f3a7536ce1188ddff8626be
Thauan Mendes <thauan.mendes@appratico.com>
2021-11-25 17:29:19 -0300

    AP-25 Criado conexao com API para cadastro de usuario

---
+++ src/webapp/assets/js/cadastro.js
@@ -0,0 +1,22 @@
+$('#formulario-cadastro').on('submit', criarUsuario);
+
+function criarUsuario(evento){
+    evento.preventDefault();
+    console.log("Dentro da função usuario");
+
+    if ($('#senha').val() != $('#confirmar-senha').val()) {
+        alert("As senhas não coincidem!");
+        return;
+    }
+
+    $.ajax({
+        url: "/web/usuario/registrar",
+        method: "POST",
+        data: {
+            nome: $('#nome').val(),
+            nick: $('#nick').val(),
+            email: $('#email').val(),
+            senha: $('#senha').val()
+        }
+    })
+}
