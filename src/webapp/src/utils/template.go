fa889de4d305090e6247c46ed33c6617ceba6249
Thauan1995 <thauanmendes13@gmail.com>
2021-11-30 22:12:10 -0300

    AP-31 Criado templates para codigos repetitivos

--- src/webapp/src/utils/template.go
+++ src/webapp/src/utils/template.go
@@ -10,6 +10,7 @@ var templates *template.Template
 //Insere os templates html na variavel templates
 func CarregarTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // Renderiza uma página html na tela
